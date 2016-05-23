39ee7aee09bb4e7fa40bf9d2326a2d3ad1b9dcb4
Laurence Syree <laurence@syree.eu>
2016-05-23 22:53:09 +0100

    copied from mem stick
    stuff added

---
+++ old/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// This code always runs upon the page finishing loading.
+	
+	// Code for making the nav-bar buttons go grey when you hover over them.
+	$(".nav-button").hover(function(){
+		$(this).css("background-color", "lightgrey");
+		}, function(){
+		$(this).css("background-color", "white");
+	}); 
+
+});
